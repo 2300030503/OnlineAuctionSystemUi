d94e7093d90813797602d61e6e9ef1a1ccd1e1dc
GOPI KRISHNA <2300030503@kluniversity.in>
2025-02-09 18:18:39 +0530

    feature/sravya

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const Dashboard = () => {
     const navigate = useNavigate();
 
+    
     const handleLogout = () => {
         AuthService.logout();
         navigate('/login');
